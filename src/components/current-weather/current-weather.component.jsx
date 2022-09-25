3fd9511d58c40c6a456eb852dc2ba7dc475bfa20
Joe Giuffrida <conquercat24@gmail.com>
2022-09-25 13:43:34 -0700

    created the weather widget in the current-weather component with static data

--- src/components/current-weather/current-weather.component.jsx
+++ src/components/current-weather/current-weather.component.jsx
@@ -2,8 +2,41 @@ import styles from './current-weather.module.css';
 
 const CurrentWeather = () => {
   return (
-    <div>
-      <h1>current weather component</h1>
+    <div className={styles.weather}>
+      <div className={styles.top}>
+        <div>
+          <p className={styles.city}>belgrade</p>
+          <p className={styles.weatherDescription}>sunny</p>
+        </div>
+        <img alt="weather" className={styles.weatherIcon} src="icons/01d.png" />
+      </div>
+
+      <div className={styles.bottom}>
+        <p className={styles.temperature}>18°C</p>
+        <div className={styles.details}>
+          <div className={styles.parameterRow}>
+            <span className={`${styles.parameterLabel} ${styles.top}`}>
+              Details
+            </span>
+          </div>
+          <div className={styles.parameterRow}>
+            <span className={styles.parameterLabel}>Feels like</span>
+            <span className={styles.parameterValue}>22°C</span>
+          </div>
+          <div className={styles.parameterRow}>
+            <span className={styles.parameterLabel}>Wind</span>
+            <span className={styles.parameterValue}>2 mph</span>
+          </div>
+          <div className={styles.parameterRow}>
+            <span className={styles.parameterLabel}>Humidity</span>
+            <span className={styles.parameterValue}>15%</span>
+          </div>
+          <div className={styles.parameterRow}>
+            <span className={styles.parameterLabel}>Pressure</span>
+            <span className={styles.parameterValue}>15</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
